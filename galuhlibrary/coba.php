0274702c94bbdb043f68ab7395ad68cdc377066b
reggyzian <reggyzianpurnama@gmail.com>
2021-08-09 20:46:56 +0700

    coba lagi nih

---
+++ galuhlibrary/coba.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="src/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <title>Galuh Library</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium animi perspiciatis modi ipsa, ab, tenetur iure quis sint perferendis, autem ducimus eum nisi nulla sed dolore amet! Error, nulla cumque.</p>
+</body>
+</html>
